2f67965316c05de8df9f29ddeca2791ac0ec1ca4
Thales Sathler <thales.lima@b2wdigital.com>
2020-03-05 11:49:09 -0300

    fix na api, ajuste do teste de aceitação

--- baas/api.py
+++ baas/api.py
@@ -6,19 +6,19 @@ from asyncworker import RouteTypes
 from baas.app import app
 from baas.http import parse_body, parse_id
 from baas.models import Transfer
-from baas.services.saque import SaqueService
+from baas.services.transfer import TransferService
 
 
-@app.route(["/transfers/"], type=RouteTypes.HTTP, methods=["POST"])
+@app.route(["/transfer/"], type=RouteTypes.HTTP, methods=["POST"])
 @parse_body(Transfer)
 async def transfer(transfer: Transfer) -> Transfer:
-    t = await TransferService.save_transfer(transfer)
+    t = await TransferService.save_transfer(transfer.origem, transfer)
     return web.json_response(t.dict())
 
 
 @app.route(["/transfers/{acc_id}"], type=RouteTypes.HTTP, methods=["GET"])
 @parse_id(str)
-async def lista_saques(acc_id: str) -> List[Transfer]:
+async def lista_transfers(acc_id: str) -> List[Transfer]:
     transfers = TransferService.list_by_origem_id(acc_id)
     return web.json_response([t.dict() for t in transfers])
 
--- tests/test_http_api.py
+++ tests/test_http_api.py
@@ -2,11 +2,9 @@ from asynctest import TestCase
 from asyncworker.testing import HttpClientContext
 
 from baas.api import app
-from baas.models import Saque
-from baas.services.saque import SaqueService
 
 
-class SaqueAPITest(TestCase):
+class TransferAPITest(TestCase):
     async def test_health(self):
         async with HttpClientContext(app) as client:
             resp = await client.get("/health")

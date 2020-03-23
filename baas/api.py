217bb92e121f3403af5d8a97d6b915df8e1322fb
Dalton Barreto <daltonmatos@gmail.com>
2020-03-23 14:41:18 -0300

    Handlers HTTP retornando modelos pydantic

--- baas/api.py
+++ baas/api.py
@@ -1,6 +1,6 @@
+from typing import List
+
 from aiohttp import web
-from asyncworker import RouteTypes
-from asyncworker.http.decorators import parse_path
 
 from baas.app import app
 from baas.http import parse_body
@@ -10,15 +10,13 @@ from baas.services.transfer import TransferService
 
 @app.http(["/transfers"], methods=["POST"])
 @parse_body(Transfer)
-async def transfer(transfer: Transfer):
-    t = await TransferService.save_transfer(transfer.origem, transfer)
-    return web.json_response(t.dict())
+async def transfer(transfer: Transfer) -> Transfer:
+    return await TransferService.save_transfer(transfer.origem, transfer)
 
 
 @app.http(["/transfers/{acc_id}"])
-async def lista_transfers(acc_id: str):
-    transfers = TransferService.list_by_origem_id(acc_id)
-    return web.json_response([t.dict() for t in transfers])
+async def lista_transfers(acc_id: str) -> List[Transfer]:
+    return TransferService.list_by_origem_id(acc_id)
 
 
 @app.http(["/health"])

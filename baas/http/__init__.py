965d9424da66616107133db6499f87021aeffc17
Dalton Barreto <daltonmatos@gmail.com>
2020-03-23 14:14:01 -0300

    Removendo baas.http.parse_id
    Essa funcionalidade já existe no asyncworker

--- baas/http/__init__.py
+++ baas/http/__init__.py
@@ -15,17 +15,3 @@ def parse_body(model: Type):
         return _wrap
 
     return _wrap_
-
-
-def parse_id(param_type: Type):
-    def _handler_wrap(f):
-        async def _wrap(wrapper: RequestWrapper):
-            req = wrapper.http_request
-            first_path_param = list(req.match_info.keys())[0]
-            param_value = param_type(req.match_info[first_path_param])
-            wrapper.types_registry.set(param_value)
-            return await call_http_handler(wrapper.http_request, f)
-
-        return _wrap
-
-    return _handler_wrap
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -25,22 +25,11 @@ class HTTPDecoratorsTest(TestCase):
         async def parse_body_handler(m: Model):
             return web.json_response(m.dict())
 
-        @self.app.http(["/get_by_id/{id}"], methods=["POST"])
-        async def parse_path_handler(id_: int):
-            m = Model(name="Dalton", numero=id_)
-            return web.json_response(m.dict())
-
     async def test_parse_body(self):
         async with HttpClientContext(self.app) as client:
             resp = await client.post(
                 "/body", json=Model(name="Dalton", numero=42).dict()
             )
-            data = await resp.json()
-            self.assertEqual({"name": "Dalton", "numero": 42}, data)
-
-    async def test_parse_path_param(self):
-        async with HttpClientContext(self.app) as client:
-            resp = await client.post("/get_by_id/42")
             data = await resp.json()
             self.assertEqual({"name": "Dalton", "numero": 42}, data)
 
